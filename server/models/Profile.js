68cd3af215951f1fbba60bfbe6db67be3bafc6be
tanishqawasthi <awasthitanishq03@gmail.com>
2023-03-25 21:17:56 +0530

    Minor changes

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -4,52 +4,52 @@ const bcrypt = require("bcryptjs")
 const ProfileSchema = new mongoose.Schema({
     fname : {
         type:String,
-        required: true,
+        required: [true, 'Please add first name'],
     },
     lname : {
         type:String,
-        required: true,
+        required: [true, 'Please add last name'],
     },
     phone : {
         type: Number,
-        required: true,
+        required: [true, 'Please add phone number'],
     },
     username : {
         type: String,
-        required: true,
+        required: [true, 'Please add username'],
         unique: true,
     },
     email : {
         type: String,
-        required: true,
+        required: [true, 'Please add email'],
     },
     address : {
         type: String,
-        required: true,
+        required: [true, 'Please add address'],
     },
     state : {
         type: String,
-        required: true,
+        required: [true, 'Please add state'],
     },
     zip : {
         type: Number,
-        required: true,
+        required: [true, 'Please add zip code'],
     },
     city : {
         type: String,
-        required: true,
+        required: [true, 'Please add city'],
     },
     password : {
         type: String,
-        required: true,
+        required: [true, 'Please add password'],
     },
     imageUrl : {
         type: String,
-        required: true,
+        required: [true, 'Please add image url'],
     },
     isAdmin: {
         type: Number,
-        require: true,
+        required: true,
     },
     adopted: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Pet" }],
     rescued: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Pet" }]
